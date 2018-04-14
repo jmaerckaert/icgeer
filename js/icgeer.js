13f330010a94510a3cf28f764810d9b9beca1e82
Jonathan Maerckaert <jmaerckaert@gmail.com>
2018-04-14 13:55:28 +0200

    Add styles

---
+++ js/icgeer.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * This file is custom js for ICGeer.
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.myBehavior = {
+    attach: function (context, settings) {
+    }
+  };
+})(jQuery, Drupal);
